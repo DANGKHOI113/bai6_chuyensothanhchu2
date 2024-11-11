1e59c723ab67db7a2d5a1c9547ce507ec1fc7f48
MAY 06 <MAY 06@LAB2-05>
2024-11-11 16:12:42 +0700

    Add project files.

---
+++ bai6_chuyensothanhchu.cpp
@@ -0,0 +1,48 @@
+// bai6_chuyensothanhchu.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int so;
+    cout << "Nhap so";
+    cin >> so;
+    if (so == 0) {
+        cout << "khong\n";
+    }
+    else if (so == 1) {
+        cout << "mot\n";
+    }
+    else if (so == 2) {
+        cout << "hai\n";
+    }
+    else if (so == 3) {
+        cout << "ba\n";
+    }
+    else if (so == 4) {
+        cout << "bon\n";
+    }
+    else if (so == 5) {
+        cout << "nam\n";
+    }
+    else if (so == 6) {
+        cout << "sau\n";
+    }
+    else if (so == 7) {
+        cout << "bay\n";
+    }
+    else if (so == 8) {
+        cout << "tam\n";
+    }
+    else if (so == 9) {
+        cout << "chin\n";
+    }
